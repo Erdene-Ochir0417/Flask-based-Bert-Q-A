10067ab7e6b9601d2303e892aa7e92f47eb8059f
rboxochir <erdeneochir0417@gmail.com>
2020-03-05 09:26:47 +0800

    japan

--- run.py
+++ run.py
@@ -2,16 +2,29 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/send', methods=['GET','POST'])
-def send():
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/japanese', methods=['GET','POST'])
+def japanese():
     if request.method == 'POST':
         context = request.form['context']
         question = request.form['question']
     
-        return render_template('output.html', context=context, question=question)
+        return render_template('japanese_output.html', context=context, question=question)
 
-    return render_template('index.html')
+    return render_template('japanese.html')
 
+@app.route('/english', methods=['GET','POST'])
+def english():
+    if request.method == 'POST':
+        context = request.form['context']
+        question = request.form['question']
+
+        return render_template('english_output.html', context=context, question=question)
+
+    return render_template('english.html')
 
 if __name__ == "__main__":
     app.run()
